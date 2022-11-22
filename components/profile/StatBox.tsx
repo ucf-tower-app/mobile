a98a343119433b7ca8c9a9c476de209ce2747bcc
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2022-11-22 07:00:56 +0000

    Remove redundant centers

--- components/profile/StatBox.tsx
+++ components/profile/StatBox.tsx
@@ -1,5 +1,5 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Pressable, Text, VStack, Center } from 'native-base';
+import { Pressable, Text, VStack } from 'native-base';
 import { DeviceEventEmitter } from 'react-native';
 import { PropMap } from '../../utils/routes/routes';
 
@@ -19,17 +19,15 @@ const StatBox = ({ route }: Props) => {
             bg={
               isPressed ? 'coolGray.100' : isHovered ? 'coolGray.100' : 'white'
             }
+            justifyContent="center"
+            alignItems="center"
           >
-            <Center>
-              <Text fontSize="xl" color="gray.400">
-                {route.params.stat}
-              </Text>
-            </Center>
-            <Center>
-              <Text fontSize="2xl" bold>
-                {route.params.value}
-              </Text>
-            </Center>
+            <Text fontSize="xl" color="gray.400">
+              {route.params.stat}
+            </Text>
+            <Text fontSize="2xl" bold>
+              {route.params.value}
+            </Text>
           </VStack>
         );
       }}
