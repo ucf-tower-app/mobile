5e0d5a4c06b903d85423c1aba7777cf1c1762d22
eb <estebancbrugal@gmail.com>
2022-11-26 23:36:16 +0000

    Create navbar component.

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,86 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Text, HStack, Center, Box, Pressable, VStack, SearchIcon, ThreeDotsIcon, Flex } from 'native-base';
+import { PropMap } from '../../utils/routes/routes';
+import { useRef, useState } from 'react';
+import { ScrollView } from 'react-native'
+
+type Props = NativeStackScreenProps<PropMap, 'Navbar'>;
+
+
+const Navbar = ({ route }: Props) => {
+    const scrollRef = useRef<ScrollView>(null);
+    const [selected, setSelected] = useState(0);
+
+    const handlePress = (index: number) => {
+        setSelected(index);
+        if (index === 3) {
+            scrollRef.current?.scrollToEnd();
+        } else if (index === 0 || index === 1) {
+            scrollRef.current?.scrollTo({ x: 0, y: 0, animated: true });
+        }
+    }
+
+    return (
+        <VStack bg="white" pb="2" shadow="2">
+            <Flex direction="row" p="2">
+                <Box w="80%">
+                    <Text fontSize="xl" bold>
+                        {selected === 0 ? "All" : selected === 1 ? "Following" : selected === 2 ? "Leaderboards" : selected === 3 ? "Lost and Found" : "ERROR"}
+                    </Text>
+                </Box>
+                <Box w="20%" justifyContent="center">
+                    <HStack justifyContent="space-around">
+                        <Center>
+                            <SearchIcon size="md" />
+                        </Center>
+                        <Center>
+                            <ThreeDotsIcon size="md" />
+                        </Center>
+                    </HStack>
+                </Box>
+            </Flex>
+            <ScrollView ref={scrollRef} horizontal={true} showsHorizontalScrollIndicator={false}>
+                <HStack bg="white" shadow={6}>
+                    <Pressable p="2" flex={1} onPress={() => handlePress(0)}>
+                        <Box rounded="lg" px="3" bg={selected === 0 ? "purple.500" : "white"}>
+                            <Center>
+                                <Text color={selected === 0 ? "muted.50" : "black"}>
+                                    All
+                                </Text>
+                            </Center>
+                        </Box>
+                    </Pressable>
+                    <Pressable p="2" flex={1} onPress={() => handlePress(1)}>
+                        <Box rounded="lg" px="3" bg={selected === 1 ? "purple.500" : "white"}>
+                            <Center>
+                                <Text color={selected === 1 ? "muted.50" : "black"}>
+                                    Following
+                                </Text>
+                            </Center>
+                        </Box>
+                    </Pressable>
+                    <Pressable p="2" flex={1} onPress={() => handlePress(2)}>
+                        <Box rounded="lg" px="3" bg={selected === 2 ? "purple.500" : "white"}>
+                            <Center>
+                                <Text color={selected === 2 ? "muted.50" : "black"}>
+                                    Leaderboards
+                                </Text>
+                            </Center>
+                        </Box>
+                    </Pressable>
+                    <Pressable p="2" flex={1} onPress={() => handlePress(3)}>
+                        <Box rounded="lg" px="3" bg={selected === 3 ? "purple.500" : "white"}>
+                            <Center>
+                                <Text color={selected === 3 ? "muted.50" : "black"}>
+                                    Lost And Found
+                                </Text>
+                            </Center>
+                        </Box>
+                    </Pressable>
+                </HStack>
+            </ScrollView>
+        </VStack >
+    );
+};
+
+export default Navbar;
--- utils/routes/sandbox/names.ts
+++ utils/routes/sandbox/names.ts
@@ -1,3 +1,3 @@
 // All valid route names for sandbox tab
-export const names = ['Sandbox', 'RouteRow', 'ProfileBanner', 'StatBox'] as const;
+export const names = ['Sandbox', 'RouteRow', 'ProfileBanner', 'StatBox', 'Navbar'] as const;
 export type Name = typeof names[number];
