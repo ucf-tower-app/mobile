4ab58002448b3d984fb28710409736047e403ccf
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2022-12-01 02:10:42 +0000

    Lint

--- components/lostandfound/LostAndFoundCard.tsx
+++ components/lostandfound/LostAndFoundCard.tsx
@@ -3,7 +3,6 @@ import { Box, VStack, Text, ChevronDownIcon, Pressable } from 'native-base';
 import { useState } from 'react';
 import { PropMap } from '../../utils/routes/routes';
 
-
 type Props = NativeStackScreenProps<PropMap, 'LostAndFoundCard'>;
 const LostAndFoundCard = ({ route }: Props) => {
   const [seeMore, setSeeMore] = useState(false);
@@ -12,11 +11,16 @@ const LostAndFoundCard = ({ route }: Props) => {
     <Box width="95%" rounded="10px" bg="muted.50" p="2">
       <VStack space="2">
         <Box px="1">
-          <Text fontSize="2xl">
-            {route.params.title}
-          </Text>
+          <Text fontSize="2xl">{route.params.title}</Text>
         </Box>
-        <Box pt="2" px="2" bg="muted.200" borderWidth="1" borderColor="muted.300" rounded="5px">
+        <Box
+          pt="2"
+          px="2"
+          bg="muted.200"
+          borderWidth="1"
+          borderColor="muted.300"
+          rounded="5px"
+        >
           <Text noOfLines={!seeMore ? 5 : undefined}>
             {route.params.description}
           </Text>
@@ -24,7 +28,17 @@ const LostAndFoundCard = ({ route }: Props) => {
             <Pressable onPress={() => setSeeMore(!seeMore)}>
               {({ isHovered, isPressed }) => {
                 return (
-                  <Box bg={isPressed ? 'coolGray.100' : isHovered ? 'coolGray.100' : 'muted.200'} rounded="5px" m="1">
+                  <Box
+                    bg={
+                      isPressed
+                        ? 'coolGray.100'
+                        : isHovered
+                        ? 'coolGray.100'
+                        : 'muted.200'
+                    }
+                    rounded="5px"
+                    m="1"
+                  >
                     <ChevronDownIcon />
                   </Box>
                 );
