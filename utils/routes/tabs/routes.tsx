f2aba9b5349adf50b2e5ce648fcfb83166985f8e
Esteban Brugal <estebancbrugal@gmail.com>
2022-12-07 19:07:23 +0000

    Switch to native base Icon. Use native base Icon for more consistency.

--- utils/routes/tabs/routes.tsx
+++ utils/routes/tabs/routes.tsx
@@ -6,6 +6,7 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import { Icon } from 'native-base';
 
 export type Route = {
   name: Name;
@@ -18,39 +19,69 @@ export const routes: Array<Route> = [
   {
     name: 'HomeTab',
     component: View,
-    focusedIcon: <MaterialCommunityIcons name="home" size={26} color="black" />,
+    focusedIcon: (
+      <Icon
+        as={<MaterialCommunityIcons name="home" />}
+        color="black"
+        size="xl"
+      />
+    ),
     unfocusedIcon: (
-      <MaterialCommunityIcons name="home-outline" size={26} color="black" />
+      <Icon
+        as={<MaterialCommunityIcons name="home-outline" />}
+        color="black"
+        size="xl"
+      />
     ),
   },
   {
     name: 'LeaderboardsTab',
     component: View,
-    focusedIcon: <Ionicons name="trophy" size={26} color="black" />,
-    unfocusedIcon: <Ionicons name="trophy-outline" size={26} color="black" />,
+    focusedIcon: (
+      <Icon as={<Ionicons name="trophy" />} color="black" size="xl" />
+    ),
+    unfocusedIcon: (
+      <Icon as={<Ionicons name="trophy-outline" />} color="black" size="xl" />
+    ),
   },
   {
     name: 'ActiveRoutesTab',
     component: View,
-    focusedIcon: <AntDesign name="questioncircle" size={26} color="black" />,
-    unfocusedIcon: <AntDesign name="questioncircleo" size={26} color="black" />,
+    focusedIcon: (
+      <Icon as={<AntDesign name="questioncircle" />} color="black" size="xl" />
+    ),
+    unfocusedIcon: (
+      <Icon as={<AntDesign name="questioncircleo" />} color="black" size="xl" />
+    ),
   },
   {
     name: 'SearchTab',
     component: View,
-    focusedIcon: <Ionicons name="search" size={26} color="black" />,
-    unfocusedIcon: <Ionicons name="search-outline" size={26} color="black" />,
+    focusedIcon: (
+      <Icon as={<Ionicons name="search" />} color="black" size="xl" />
+    ),
+    unfocusedIcon: (
+      <Icon as={<Ionicons name="search-outline" />} color="black" size="xl" />
+    ),
   },
   {
     name: 'ProfileTab',
     component: View,
-    focusedIcon: <Ionicons name="person" size={26} color="black" />,
-    unfocusedIcon: <Ionicons name="person-outline" size={26} color="black" />,
+    focusedIcon: (
+      <Icon as={<Ionicons name="person" />} color="black" size="xl" />
+    ),
+    unfocusedIcon: (
+      <Icon as={<Ionicons name="person-outline" />} color="black" size="xl" />
+    ),
   },
   {
     name: 'SandboxTab',
     component: Sandbox,
-    focusedIcon: <Feather name="codesandbox" size={26} color="black" />,
-    unfocusedIcon: <Feather name="codesandbox" size={26} color="black" />,
+    focusedIcon: (
+      <Icon as={<Feather name="codesandbox" />} color="black" size="xl" />
+    ),
+    unfocusedIcon: (
+      <Icon as={<Feather name="codesandbox" />} color="black" size="xl" />
+    ),
   },
 ];
