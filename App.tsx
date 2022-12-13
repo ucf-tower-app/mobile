99230418fd2dc2d36087f091486528460f3b9f32
Esteban Brugal <estebancbrugal@gmail.com>
2022-12-13 03:12:17 +0000

    Update App.tsx.
    Don't need buildStack anymore since the stacks were created in their own file.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, extendTheme } from 'native-base';
-import { tabNameToRouteData } from './utils/routes/routes';
-import { Name as TabName } from './utils/routes/tabs/names';
 import { routes as tabRoutes } from './utils/routes/tabs/routes';
+import { ParamList as RootTabParamList } from './utils/routes/tabs/paramList';
 
 // Style for tab bar
 const tabBarStyle = {
@@ -13,26 +11,7 @@ const tabBarStyle = {
 };
 
 // Tabs used for bottom tray, stack for in-tab nav
-const Stack = createNativeStackNavigator();
-const Tabs = createMaterialBottomTabNavigator();
-
-// Builds a navigator stack for a given tab
-const buildStack = (tabName: TabName) => {
-  const routeData = tabNameToRouteData[tabName];
-  return () => {
-    return (
-      <Stack.Navigator initialRouteName={routeData.initialRouteName}>
-        {routeData.routes.map((route) => (
-          <Stack.Screen
-            name={route.name}
-            component={route.component}
-            key={route.name}
-          />
-        ))}
-      </Stack.Navigator>
-    );
-  };
-};
+const Tabs = createMaterialBottomTabNavigator<RootTabParamList>();
 
 const theme = extendTheme({
   colors: {
@@ -56,14 +35,14 @@ export default function App() {
       <NavigationContainer>
         <StatusBar style="auto" />
         <Tabs.Navigator
-          initialRouteName="Home"
+          initialRouteName="HomeTab"
           barStyle={tabBarStyle}
           labeled={false}
         >
           {tabRoutes.map((route) => (
             <Tabs.Screen
               name={route.name}
-              component={buildStack(route.name)}
+              component={route.component}
               options={{
                 tabBarIcon: ({ focused }) =>
                   focused ? route.focusedIcon : route.unfocusedIcon,
