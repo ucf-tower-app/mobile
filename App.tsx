114f05645ea742f2d6445b3c0a43c81651d6cbf6
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2023-01-09 18:23:18 -0500

    Work from in person with Esteban

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, extendTheme } from 'native-base';
-import { tabNameToRouteData } from './utils/routes/routes';
-import { routes as tabRoutes } from './utils/routes/tabs/routes';
-import { Name as TabName } from './utils/routes/tabs/names';
-import { ParamList as RootTabParamList } from './utils/routes/tabs/paramList';
+import { extendTheme, NativeBaseProvider } from 'native-base';
 import 'react-native-gesture-handler';
+import { ParamList as RootTabParamList } from './utils/routes/tabs/paramList';
+import { routes as tabRoutes } from './utils/routes/tabs/routes';
 
 // Style for tab bar
 const tabBarStyle = {
@@ -15,25 +14,6 @@ const tabBarStyle = {
 
 // Tabs used for bottom tray, stack for in-tab nav
 const Tabs = createMaterialBottomTabNavigator<RootTabParamList>();
-
-// Builds a navigator stack for a given tab
-const buildStack = (tabName: TabName, stack: any) => {
-  const routeData = tabNameToRouteData[tabName];
-  const Stack = stack;
-  return () => {
-    return (
-      <Stack.Navigator initialRouteName={routeData.initialRouteName}>
-        {routeData.routes.map((route) => (
-          <Stack.Screen
-            name={route.name}
-            component={route.component}
-            key={route.name}
-          />
-        ))}
-      </Stack.Navigator>
-    );
-  };
-};
 
 const theme = extendTheme({
   colors: {
@@ -50,29 +30,42 @@ const theme = extendTheme({
   },
 });
 
+const RootStack = createNativeStackNavigator();
+
+const BuildTabs = () => {
+  return (
+    <Tabs.Navigator
+      initialRouteName="HomeTab"
+      barStyle={tabBarStyle}
+      labeled={false}
+    >
+      {tabRoutes.map((route) => (
+        <Tabs.Screen
+          name={route.name}
+          component={route.stack}
+          options={{
+            tabBarIcon: ({ focused }) =>
+              focused ? route.focusedIcon : route.unfocusedIcon,
+          }}
+          key={route.name}
+        />
+      ))}
+    </Tabs.Navigator>
+  );
+};
+
 // Construct tabs and their subtrees
 export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
       <NavigationContainer>
         <StatusBar style="auto" />
-        <Tabs.Navigator
-          initialRouteName="HomeTab"
-          barStyle={tabBarStyle}
-          labeled={false}
+        <RootStack.Navigator
+          initialRouteName="root"
+          screenOptions={{ headerShown: false }}
         >
-          {tabRoutes.map((route) => (
-            <Tabs.Screen
-              name={route.name}
-              component={buildStack(route.name, route.stack)}
-              options={{
-                tabBarIcon: ({ focused }) =>
-                  focused ? route.focusedIcon : route.unfocusedIcon,
-              }}
-              key={route.name}
-            />
-          ))}
-        </Tabs.Navigator>
+          <RootStack.Screen name="root" component={BuildTabs} />
+        </RootStack.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
   );
