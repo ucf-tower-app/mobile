dbf30beaa1da4fb29b00599f346ef4cb0a6d573a
Esteban Brugal <estebancbrugal@gmail.com>
2023-01-11 17:02:23 +0000

    Create HeaderMenu component.

---
+++ components/header/HeaderMenu.tsx
@@ -0,0 +1,39 @@
+import { HStack, Icon, Menu, Pressable } from 'native-base';
+import { Entypo } from '@expo/vector-icons';
+
+type Props = {
+  navigate: Function;
+};
+
+const PressableIcon = (triggerProps: any) => {
+  return (
+    <Pressable {...triggerProps}>
+      <Icon
+        as={<Entypo name="dots-three-horizontal" />}
+        color="black"
+        size="lg"
+      />
+    </Pressable>
+  );
+};
+
+const HeaderMenu = ({ navigate }: Props) => {
+  return (
+    <HStack space={3}>
+      <Menu
+        trigger={(triggerProps) => {
+          return PressableIcon(triggerProps);
+        }}
+      >
+        <Menu.Item onPress={() => navigate('Settings')}>Settings</Menu.Item>
+        <Menu.Item onPress={() => navigate('Tutorial')}>Tutorial</Menu.Item>
+        <Menu.Item onPress={() => navigate('LostAndFound')}>
+          Lost and Found
+        </Menu.Item>
+        <Menu.Item>Logout</Menu.Item>
+      </Menu>
+    </HStack>
+  );
+};
+
+export default HeaderMenu;
