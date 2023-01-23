ab7a9e3d376036fd3df6f4911caf9311dea633df
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2023-01-22 22:14:06 -0500

    Create toggleable like icon

---
+++ components/misc/IconToggle.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+
+type Props = {
+  status: boolean;
+  onToggle: () => void;
+  onIcon: JSX.Element;
+  offIcon: JSX.Element;
+};
+const IconToggle = ({ status, onToggle, onIcon, offIcon }: Props) => {
+  const [statusLocal, setStatusLocal] = useState<boolean>(status);
+
+  const toggle = async () => {
+    setStatusLocal(!statusLocal);
+    onToggle();
+  };
+
+  return (
+    <TouchableOpacity onPress={toggle}>
+      {statusLocal ? onIcon : offIcon}
+    </TouchableOpacity>
+  );
+};
+
+export default IconToggle;
