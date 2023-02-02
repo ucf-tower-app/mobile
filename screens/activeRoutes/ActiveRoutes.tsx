8b64cc8b2f30e49aa74e32b89d5620e5082e0d49
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2023-02-02 00:00:00 -0500

    Move activeRoutes to new api

--- screens/activeRoutes/ActiveRoutes.tsx
+++ screens/activeRoutes/ActiveRoutes.tsx
@@ -1,37 +1,35 @@
 import { Box, Flex, Spinner, useColorModeValue } from 'native-base';
-import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import RouteContainer from '../../components/route/SearchableRouteContainer';
-import { getActiveRoutesCursor } from '../../xplat/api';
-import { Route } from '../../xplat/types/route';
+import {
+  ACTIVE_ROUTES_CACHE_KEY,
+  fetchActiveRoutes,
+} from '../../utils/queries';
 
 const ActiveRoutes = () => {
-  const [routes, setRoutes] = useState<Route[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
   const baseBgColor = useColorModeValue('lightMode.base', 'darkMode.base');
 
-  useEffect(() => {
-    const getData = async () => {
-      // There will never be many active routes, so pull them all for searchability
-      const activeRoutesCursor = getActiveRoutesCursor();
-      const newRoutes: Route[] = [];
-      while (await activeRoutesCursor.hasNext()) {
-        newRoutes.push((await activeRoutesCursor.pollNext())!);
-      }
-      setRoutes(newRoutes);
-      setIsLoading(false);
-    };
+  const { isLoading, isError, data, error } = useQuery(
+    ACTIVE_ROUTES_CACHE_KEY,
+    fetchActiveRoutes
+  );
 
-    getData();
-  }, []);
+  if (isLoading) {
+    return (
+      <Flex w="full" h="full" justifyContent="center" alignItems="center">
+        <Spinner size="lg" />
+      </Flex>
+    );
+  }
 
-  return isLoading ? (
-    <Flex w="full" h="full" justifyContent="center" alignItems="center">
-      <Spinner size="lg" />
-    </Flex>
-  ) : (
+  if (isError || data === undefined) {
+    console.error(error);
+    return null;
+  }
+
+  return (
     <Box w="full" h="full" bg={baseBgColor}>
-      <RouteContainer routes={routes} />
+      <RouteContainer fetchedRoutes={data.activeRoutes} />
     </Box>
   );
 };
