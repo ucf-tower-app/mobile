5e45e4ded7af704b36b6588e2195ec558a55ffb2
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2023-02-06 21:13:55 -0500

    Config

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,4 @@
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs'],
-  },
-};
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+module.exports = getDefaultConfig(__dirname);
