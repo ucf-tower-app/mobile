51c04a705ea879059a35112a79ebfaa7068e430b
Jacob S <jakesteinebronn@gmail.com>
2023-02-09 11:49:40 -0500

    Usable post preview w/ images

--- components/media/MediaCarousel.tsx
+++ components/media/MediaCarousel.tsx
@@ -1,4 +1,11 @@
-import { Box, Center, HStack, useColorModeValue, VStack } from 'native-base';
+import {
+  Box,
+  Center,
+  HStack,
+  Text,
+  VStack,
+  useColorModeValue,
+} from 'native-base';
 import { useEffect, useState } from 'react';
 import { Dimensions, Image as ImageRN } from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
@@ -6,8 +13,9 @@ import Media, { MediaType } from './Media';
 
 type Props = {
   mediaList: MediaType[];
+  preview?: boolean;
 };
-const MediaCarousel = ({ mediaList }: Props) => {
+const MediaCarousel = ({ mediaList, preview = false }: Props) => {
   const primaryColor = useColorModeValue(
     'lightMode.primary',
     'darkMode.primary'
@@ -38,21 +46,30 @@ const MediaCarousel = ({ mediaList }: Props) => {
 
   return (
     <VStack space={2} width={width}>
-      <Carousel
-        loop={false}
-        data={data}
-        width={width}
-        height={height}
-        renderItem={({ index }) => (
-          <Media media={mediaList[index]} width={width} height={height} />
-        )}
-        onProgressChange={(_, absoluteProgress) =>
-          setFocusedIndex(Math.round(absoluteProgress))
-        }
-        panGestureHandlerProps={{
-          activeOffsetX: [-10, 10],
-        }}
-      />
+      {preview && (
+        <Center>
+          <Text fontSize={'xs'} color={'gray.400'} textAlign={'center'}>
+            (This is a preview; post images/videos will be full-size.)
+          </Text>
+        </Center>
+      )}
+      <Center>
+        <Carousel
+          loop={false}
+          data={data}
+          width={width * (preview ? 0.5 : 1)}
+          height={height * (preview ? 0.5 : 1)}
+          renderItem={({ index }) => (
+            <Media media={mediaList[index]} width={width} height={height} />
+          )}
+          onProgressChange={(_, absoluteProgress) =>
+            setFocusedIndex(Math.round(absoluteProgress))
+          }
+          panGestureHandlerProps={{
+            activeOffsetX: [-10, 10],
+          }}
+        />
+      </Center>
       <Center>
         <HStack space={1.5}>
           {data.map((index) => (
