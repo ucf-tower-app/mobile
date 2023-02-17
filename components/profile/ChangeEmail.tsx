034de3f56538f30f3fe18e7200da516a6165cc42
Esteban Brugal <estebancbrugal@gmail.com>
2023-02-17 16:03:09 +0000

    Create change email modal. Add modal to header menu if user's status is verified.

--- components/profile/ChangeEmail.tsx
+++ components/profile/ChangeEmail.tsx
@@ -1,7 +1,45 @@
-import { Text } from 'native-base';
+import { Button, Modal } from 'native-base';
+import { useRecoilValue } from 'recoil';
+import { userAtom } from '../../utils/atoms';
+import { userPermissionLevelAtom } from '../../utils/atoms';
+import { useSetRecoilState } from 'recoil';
+import { UserStatus } from '../../xplat/types';
 
-const ChangeEmail = () => {
-  return <Text>Trying to change email</Text>;
+type Props = {
+  isConfirming: boolean;
+  close: () => void;
+};
+const ChangeEmail = ({ isConfirming, close }: Props) => {
+  const setUserPermissionLevel = useSetRecoilState(userPermissionLevelAtom);
+  const signedInUser = useRecoilValue(userAtom);
+
+  const verify = () => {
+    return;
+    if (signedInUser === undefined) return;
+    setUserPermissionLevel(UserStatus.Unverified);
+    close();
+  };
+
+  return (
+    <Modal isOpen={isConfirming} onClose={close}>
+      <Modal.Content maxWidth="lg">
+        <Modal.CloseButton />
+        <Modal.Header>Change Email</Modal.Header>
+        <Modal.Body>
+          This will lock you out of your account until you verify the new email.
+          Are you sure you want to do this?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onPress={close} variant="unstyled" colorScheme="coolGray">
+            Cancel
+          </Button>
+          <Button onPress={verify} colorScheme="danger">
+            Change Email
+          </Button>
+        </Modal.Footer>
+      </Modal.Content>
+    </Modal>
+  );
 };
 
 export default ChangeEmail;
--- components/profile/EditProfileModal.tsx
+++ components/profile/EditProfileModal.tsx
@@ -122,93 +122,99 @@ function EditProfileModal({ isOpen, onClose, fetchedUser }: Props) {
   };
 
   return (
-    <Modal isOpen={isOpen} onClose={handleCancel}>
-      <Modal.Content maxWidth="lg">
-        <Modal.CloseButton />
-        <Modal.Header>Edit Profile</Modal.Header>
-        <Modal.Body>
-          {viewChangePassword ? (
-            <ChangePassword
-              setOldPwd={setOldPwd}
-              setNewPwd={setNewPwd}
-              setConfirmPwd={setConfirmPwd}
-              newPwd={newPwd}
-              confirmPwd={confirmPwd}
-            />
-          ) : viewChangeEmail ? (
-            <ChangeEmail />
-          ) : (
-            <Box>
-              <FormControl isInvalid={invalidDisplayName}>
-                <FormControl.Label>Name</FormControl.Label>
-                <Input
-                  placeholder={signedInUser?.displayName}
-                  onChangeText={(e) =>
-                    dispNameSession.trigger(() => setNewDisplayName(e.trim()))
-                  }
-                />
-                <FormControl.ErrorMessage>
-                  Display Name must be 5-30 Upper/Lowercase letters, spaces, and
-                  hyphens
-                </FormControl.ErrorMessage>
-              </FormControl>
-              <FormControl mt="3" isInvalid={invalidBio}>
-                <FormControl.Label>Bio</FormControl.Label>
-                <Input
-                  placeholder={signedInUser?.bio}
-                  onChangeText={(e) =>
-                    bioSession.trigger(() => setNewBio(e.trim()))
-                  }
-                />
-                <FormControl.ErrorMessage>
-                  Bio must be at most 200 characters
-                </FormControl.ErrorMessage>
-              </FormControl>
-              <Center>
-                <UploadImage
-                  editAvatar={editAvatar}
-                  setEditAvatar={setEditAvatar}
-                />
-              </Center>
-              <HStack pt="3" space="xs" justifyContent="space-between">
-                <Button
-                  onPress={() => {
-                    setViewChangePassword(true);
-                  }}
-                  size="sm"
-                  bg={secondaryBgColor}
-                >
-                  Change Password
-                </Button>
-                <Button
-                  onPress={() => {
-                    setViewChangeEmail(true);
-                  }}
-                  size="sm"
-                  bg={secondaryBgColor}
-                >
-                  Change Email
-                </Button>
-              </HStack>
-            </Box>
-          )}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button.Group space={2}>
-            <Button
-              variant="ghost"
-              colorScheme="blueGray"
-              onPress={handleCancel}
-            >
-              Cancel
-            </Button>
-            <Button bg={secondaryBgColor} onPress={handleSave}>
-              Save
-            </Button>
-          </Button.Group>
-        </Modal.Footer>
-      </Modal.Content>
-    </Modal>
+    <Box>
+      <Modal isOpen={isOpen} onClose={handleCancel}>
+        <Modal.Content maxWidth="lg">
+          <Modal.CloseButton />
+          <Modal.Header>Edit Profile</Modal.Header>
+          <Modal.Body>
+            {viewChangePassword ? (
+              <ChangePassword
+                setOldPwd={setOldPwd}
+                setNewPwd={setNewPwd}
+                setConfirmPwd={setConfirmPwd}
+                newPwd={newPwd}
+                confirmPwd={confirmPwd}
+              />
+            ) : (
+              <Box>
+                <FormControl isInvalid={invalidDisplayName}>
+                  <FormControl.Label>Name</FormControl.Label>
+                  <Input
+                    placeholder={signedInUser?.displayName}
+                    onChangeText={(e) =>
+                      dispNameSession.trigger(() => setNewDisplayName(e.trim()))
+                    }
+                  />
+                  <FormControl.ErrorMessage>
+                    Display Name must be 5-30 Upper/Lowercase letters, spaces,
+                    and hyphens
+                  </FormControl.ErrorMessage>
+                </FormControl>
+                <FormControl mt="3" isInvalid={invalidBio}>
+                  <FormControl.Label>Bio</FormControl.Label>
+                  <Input
+                    placeholder={signedInUser?.bio}
+                    onChangeText={(e) =>
+                      bioSession.trigger(() => setNewBio(e.trim()))
+                    }
+                  />
+                  <FormControl.ErrorMessage>
+                    Bio must be at most 200 characters
+                  </FormControl.ErrorMessage>
+                </FormControl>
+                <Center>
+                  <UploadImage
+                    editAvatar={editAvatar}
+                    setEditAvatar={setEditAvatar}
+                  />
+                </Center>
+                <HStack pt="3" space="xs" justifyContent="space-between">
+                  <Button
+                    onPress={() => {
+                      setViewChangePassword(true);
+                    }}
+                    size="sm"
+                    bg={secondaryBgColor}
+                  >
+                    Change Password
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      setViewChangeEmail(true);
+                    }}
+                    size="sm"
+                    bg={secondaryBgColor}
+                  >
+                    Change Email
+                  </Button>
+                </HStack>
+              </Box>
+            )}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="ghost"
+                colorScheme="blueGray"
+                onPress={handleCancel}
+              >
+                Cancel
+              </Button>
+              <Button bg={secondaryBgColor} onPress={handleSave}>
+                Save
+              </Button>
+            </Button.Group>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
+      <ChangeEmail
+        isConfirming={viewChangeEmail}
+        close={() => {
+          setViewChangeEmail(false);
+        }}
+      />
+    </Box>
   );
 }
 
