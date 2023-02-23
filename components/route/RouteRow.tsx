d37fc893fa34c1ae295cd702c015b3b09ce8f528
Jacob Steinebronn <jakesteinebronn@gmail.com>
2023-02-22 20:27:26 -0500

    Potential UI change for RouteRow

--- components/route/RouteRow.tsx
+++ components/route/RouteRow.tsx
@@ -1,22 +1,48 @@
 import { useNavigation } from '@react-navigation/native';
 import {
   ArrowForwardIcon,
+  Box,
   HStack,
-  Image,
   Pressable,
   Skeleton,
   Text,
-  VStack,
   useColorModeValue,
+  VStack,
 } from 'native-base';
 import { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import { TabGlobalNavigationProp } from '../../utils/types';
-import { Route } from '../../xplat/types';
+import { Route, RouteColor } from '../../xplat/types';
 
 type Props = {
   route: Route;
 };
+
+function getTapeColors(routeColor: RouteColor | undefined) {
+  switch (routeColor) {
+    case RouteColor.Red:
+      return { tint: 'rgba(255,0,0,0.8)', text: 'black' };
+    case RouteColor.Pink:
+      return { tint: 'rgba(255,148,242,0.8)', text: 'black' };
+    case RouteColor.Orange:
+      return { tint: 'rgba(255,168,9,0.8)', text: 'black' };
+    case RouteColor.Yellow:
+      return { tint: 'rgba(255,245,0,1)', text: 'black' };
+    case RouteColor.Green:
+      return { tint: 'rgba(13,200,0,0.8)', text: 'black' };
+    case RouteColor.Blue:
+      return { tint: 'rgba(53,69,255,0.8)', text: 'white' };
+    case RouteColor.Purple:
+      return { tint: 'rgba(202,35,225,0.8)', text: 'white' };
+    case RouteColor.Brown:
+      return { tint: 'rgba(166,107,0,0.8)', text: 'white' };
+    case RouteColor.Grey:
+      return { tint: 'rgba(170,170,170,0.8)', text: 'black' };
+    default:
+      return { tint: 'black', text: 'white' };
+  }
+}
+
 const RouteRow = ({ route }: Props) => {
   const navigation = useNavigation<TabGlobalNavigationProp>();
 
@@ -33,6 +59,8 @@ const RouteRow = ({ route }: Props) => {
   useEffect(() => {
     if (data === undefined) return;
     let newDescriptors = data.gradeDisplayString;
+    if (data.rope) newDescriptors += ' on rope ' + data.rope;
+    if (data.naturalRules) newDescriptors += ', ' + data.naturalRules;
     if (data.stringifiedTags !== '')
       newDescriptors += ', ' + data.stringifiedTags;
     setDescriptors(newDescriptors);
@@ -57,6 +85,8 @@ const RouteRow = ({ route }: Props) => {
     return null;
   }
 
+  const tapeTint = getTapeColors(data.color as RouteColor | undefined).tint;
+
   return (
     <Pressable
       onPress={() =>
@@ -69,17 +99,31 @@ const RouteRow = ({ route }: Props) => {
         justifyContent="flex-start"
         alignItems="center"
       >
-        <Image
-          size={16}
-          borderRadius={5}
-          source={{ uri: data.thumbnailUrl }}
-          alt={data.name}
+        <Box
+          height={'17px'}
+          style={{ aspectRatio: 1 }}
+          bgColor={tapeTint}
+          borderRadius={'50%'}
+          shadow={4}
         />
-        <VStack pl={2} pt={2} maxW="70%">
-          <Text fontSize="xl" fontWeight="bold">
+        <VStack pl={2} pt={2} width="85%">
+          <Text
+            ml={2}
+            numberOfLines={1}
+            width="full"
+            fontSize="xl"
+            fontWeight="bold"
+          >
             {data.name}
           </Text>
-          <Text fontSize="lg" color="grey">
+
+          <Text
+            pl={5}
+            numberOfLines={1}
+            width="full"
+            fontSize="lg"
+            color="gray.700"
+          >
             {descriptors}
           </Text>
         </VStack>
